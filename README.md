7b50eb9250aaf01432970c93378886ce3aa712c3
neha <nehakashyap1011@gmail.com>
2024-03-14 23:04:46 +0530

    started creating netflix gpt

---
+++ README.md
@@ -0,0 +1,15 @@
+ # Features
+ - Login/Sign Up
+    - Sign In / Sign up Form
+    - redirect to Browse Page
+- Browse(after Authetication)
+    -Header
+    -Main Movie
+       - Tailer in Background
+       - Title and Description
+       - Movie Suggestion
+          - MovieList * N
+
+- NetflixGPT
+    - Search Bar
+     - Movie Suggestions
---
+++ src/components/Body.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Login from './Login'
+import Browse from './Browse'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+// import {   RouterProvider,   createBrowserRouter } from 'react-router-dom'
+
+
+const Body = () => {
+//     const appRouter = createBrowserRouter([{
+//         path:"/",
+//         element:<Login />,
+//     },
+// {
+//     path:"/browse",
+//     element:<Browse/>
+// }]);
+  return (
+    <div>
+        {/* <RouterProvider  router={appRouter} />  */}
+        <BrowserRouter>
+        <Routes>
+            <Route path ="/"  element ={<Login />} />
+            <Route path ="/browse"  element ={<Browse />} />
+        </Routes>
+        </BrowserRouter>
+
+    </div>
+  )
+}
+
+export default Body
