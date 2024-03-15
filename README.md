68656f9dd0baab270cb7519c3f7f1ba13f6627ca
neha <nehakashyap1011@gmail.com>
2024-03-15 17:00:07 +0530

    created and toggle signin and signup form

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+ 
+ # Netflix GPT
+ - created React App
+ - configure tailwind css
+ - Header
+ - Login form
+ - Sign Up form
+
+
+
+ 
  # Features
  - Login/Sign Up
     - Sign In / Sign up Form
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
  
 import Header from './Header';
  
 
 const Login = () => {
+  const [isSignInForm, SetIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    SetIsSignInForm(!isSignInForm);
+
+  }
   return (
 
     <div>
         
        
         <Header/>
-        <div>
+        <div className="absolute">
             <img src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg" /> 
         </div>
+        <form className="bg-black p-4  absolute w-3/12 mx-auto my-36 left-0 right-0 text-white bg-opacity-80 rounded-lg">
+          <h1 className="text-3xl my-4">{isSignInForm?"Sign In":"Sign Up"}</h1>
+          {!isSignInForm && (
+             <input type="text" placeholder="Full Name" className="my-2 p-3 w-full bg-[#131517]"/>
+          )}
+          <input type="text" placeholder="Email Address"  className="my-2 p-3 w-full bg-[#131517]"/>
+          <input type="password" placeholder="Password" className="my-2 p-3 w-full bg-[#131517]"/>
+          <button className="text-white bg-red-700 my-2 p-2 w-full rounded-lg"> {isSignInForm?"Sign In":"Sign Up"}</button>
+
+          <p className="py-6 cursor-pointer" onClick={toggleSignInForm}> {isSignInForm?"New to Netflix? Sign up now":"Already registered? Sign in now"}</p>
+        </form>
         </div>
   )
 }
