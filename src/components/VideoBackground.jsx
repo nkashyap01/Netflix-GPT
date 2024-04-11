7c547b3fa75b30bcd994774b397d4bc8163c126f
neha <nehakashyap1011@gmail.com>
2024-04-11 18:09:13 +0530

    made custom hook for movie trailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
+import React from "react";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+import { useSelector } from "react-redux";
 
 const VideoBackground = ({ movieId }) => {
-  const getNewMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/1011985/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-  };
-  useEffect(() => {
-    getNewMovies();
-  }, []);
-  return <div>VideoBackground</div>;
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  useMovieTrailer(movieId); // custom hook to fetch the video when component mount
+
+  return (
+    <div className="mt-4">
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
